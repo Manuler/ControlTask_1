25cdbd7a00c637df7d752b5c584bfb6fcf4f5a1f
Anton Manuylov <killsonic4@gmail.com>
2022-10-17 09:59:23 +0300

    Add project files.

---
+++ ControlTask_1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ControlTask_1
+{
+    internal class Program
+    {
+
+        public static string FindSymLengthOfThree(string[] array)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    result = result + array[i] + " ";
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string[] array = { "\"hello\"", "\"world\"", "\"2\"", "\")\"" };
+
+
+            Console.WriteLine($"Элементы, длина которых меньше либо равна 3 символа:  {FindSymLengthOfThree(array)}");
+        }
+    }
+}
